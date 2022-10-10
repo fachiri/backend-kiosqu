5caff53fae4235ab732bdc263f3a75a5f29b487e
fachiri <fachrycooles@gmail.com>
2022-10-10 23:21:31 +0800

    change listen port

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.use(cors())
 const router = require('./routes/router.js')
 app.use('/api', router)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`, this.address().port, app.settings.env))
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`, this.address().port, app.settings.env))
+app.listen(PORT, () => console.log("Express server listening on port 3000", this.address().port, app.settings.env))
